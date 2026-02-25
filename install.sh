51affbb2a2b29201e2e72196899b2bc7b487657d
kh <kylemhouseholder@gmail.com>
2026-02-25 08:38:41 -0600

    RENAME: rescope to make more fully-featured CLI tool

--- install.sh
+++ install.sh
@@ -7,18 +7,19 @@ NC='\033[0m'
 
 OK() { echo -e "${G}✓${NC} $1"; }
 
-echo "Installing script to local environment..."
+echo "Installing script..."
 
 if [[ -d $LOCAL_BIN ]]; then
 	OK "Found ${LOCAL_BIN}"
 fi
 
-if [[ -f "${LOCAL_BIN}/afd" ]]; then
+if [[ -f "${LOCAL_BIN}/avwx" ]]; then
 	OK "File already exists in local executable dir: ${G}${LOCAL_BIN}${NC}"
 else
-	cp afd ${LOCAL_BIN}
+	cp avwx ${LOCAL_BIN}
 	OK "Copied to user local executable dir: ${LOCAL_BIN}"
 fi
 
-OK "Use with '${G}afd [station]${NC}' or set default in ${G}${HOME}/.config/afd.cfg${NC}"
+OK "Use with '${G}avwx [command] [station]${NC}'"
+echo -e "   or set default in ${G}${HOME}/.config/avwx.cfg${NC}"
 
